568d79b8cfb785ae714def0b2f15629fb960f44d
yoen <jendral.hxr@gmail.com>
2023-02-07 12:54:56 +0900

    fix typos in the build script

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from distutils.core import setup
 setup(
     name="cellcv",
     version="0.0",
-    author="jendralhxr, m172205", "d191761"
-    packages=["bondas"],
+    author="jendralhxr, m172205, d191761",
+    packages=["cellcv"],
     url="https://github.com/jendralhxr/uget2",
     license="MIT License",
     author_email="zulhaj@hiroshima-u.ac.jp",
     description="Cell counting and motility evaluation estimation on from video data",
     python_requires='>=3.7',
-    install_requires.extend([
-        'opencv-python>=4.6, <1',
-        'numpy>=1.15, <2',
-        'matplotlib>=3.4 <3' 
-    ])
+	install_requires = [
+    'opencv-python>=4.6',
+    'numpy>=1.15',
+    'matplotlib>=3.4',
+	]
 )
