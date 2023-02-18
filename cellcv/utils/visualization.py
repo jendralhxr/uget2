62f8b9bb20d3cd202c2d5fb87c040a2aed7458f9
satrialoka <satrialoka@gmail.com>
2023-02-18 21:18:28 +0100

    black

--- cellcv/utils/visualization.py
+++ cellcv/utils/visualization.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import csv
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -14,7 +15,8 @@ def read_csv(csv_path: str) -> list:
     with open(csv_path, newline="") as csvfile:
         return list(csv.DictReader(csvfile, delimiter=","))
 
-def moving_average(a, n) :
+
+def moving_average(a, n):
     """
     Calculates the moving average of an array.
 
@@ -25,19 +27,27 @@ def moving_average(a, n) :
     """
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+    return ret[n - 1 :] / n
 
-def plot_1d(csv_path: str, x_key: str, y_key: str, winsize:int = 300, save_path: str = None, show_result: bool = True):
+
+def plot_1d(
+    csv_path: str,
+    x_key: str,
+    y_key: str,
+    winsize: int = 300,
+    save_path: str = None,
+    show_result: bool = True,
+):
     """
     Plot a 1D graph from a CSV file.
 
     :param csv_path: Path to the CSV file.
     :param x_key: Name of the column to be used as the x-axis.
     :param y_key: Name of the column to be used as the y-axis.
-    :param winsize: Size of the moving window for smoothing the data. Default is 300.
-    :param save_path: Optional path to save the plot as an image. If None, the plot will not be saved.
-    :param show_result: Whether or not to display the plot on the screen. Default is True.
-    
+    :param winsize: Size of the moving window for smoothing the data. 
+    :param save_path: Optional path to save the plot as an image. 
+    :param show_result: Display the plot on the screen. 
+
     :return: None
     """
     result = read_csv(csv_path)
@@ -49,16 +59,17 @@ def plot_1d(csv_path: str, x_key: str, y_key: str, winsize:int = 300, save_path:
         time_arr.append(res[x_key])
         density_arr.append(res[y_key])
 
-    plt.plot(moving_average(time_arr,winsize), moving_average(density_arr,winsize))
+    plt.plot(moving_average(time_arr, winsize), moving_average(density_arr, winsize))
     plt.xlabel(x_key)
     plt.ylabel(y_key)
-    
+
     if show_result:
         plt.show()
         plt.close()
 
     if save_path:
         plt.savefig(save_path)
+
 
 def plot_heatmap():
     return NotImplementedError
