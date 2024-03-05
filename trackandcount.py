20dd77420d0118caddc3e5d103cd4ee8f9afe765
jendralhxr <jendral.hxr@gmail.com>
2024-03-05 17:13:43 +0700

    heatmap works w/ jet colormap

--- trackandcount.py
+++ trackandcount.py
@@ -3,14 +3,14 @@
 import numpy as np
 import cv2 as cv
 import sys
-import ffmpegcv
 
 # heatmap
-COEF_FADE_IN = 1.2
-COEF_FADE_OUT = 0.4
+COEF_FADE_IN  = 1.00
+COEF_FADE_OUT = 0.75
+HEATMAP_CEIL= COEF_FADE_IN * 60 # persistence presence is noted within 1 second
 # buat LOKA: any way to set these values more gracefully? like a slider?
 
-threshold_value = 14 # good place to start
+threshold_value = 13 # good place to start
 space= 4 # most probable uget2 uget2 size
 
 framenum = 0
@@ -29,12 +29,14 @@ cap.set(cv.CAP_PROP_POS_FRAMES, float(startframe))
 framenum = startframe;
 lastframe = int(sys.argv[3])
 
-vid_cue = ffmpegcv.VideoWriter(sys.argv[4], None, 60, (width, height))
-vid_heat = ffmpegcv.VideoWriter(sys.argv[5], None, 60, (width, height))
+vid_cue = cv.VideoWriter(sys.argv[4], cv.VideoWriter_fourcc(*'mp4v'), fps, (640, 480))
+#vid_heat = cv.VideoWriter(sys.argv[5], cv.VideoWriter_fourcc(*'mp4v'), fps, (640, 480))
 
-heatmap = np.zeros([height, width], dtype=np.uint8)
-mask = np.full([height, width], 255, dtype=np.uint8)
+heatmap = np.zeros([height, width], dtype=np.single)
+heatmap_cue = np.full([height, width], 255, dtype=np.uint8)
+
 mask_col = np.full([height, width, 3], (255,255,255), dtype=np.uint8)
+mask = np.full([height, width], 255, dtype=np.uint8)
 
 #------ setting the capilary mask
 key=2;
@@ -74,7 +76,7 @@ while (key != ord('s')):
 # --- main routine
 while (framenum < lastframe) and (framenum < frame_length - 1):
     ret, current_col = cap.read()
-    vid_cue.write(current_col)
+    #vid_cue.write(current_col)
     
     current = cv.cvtColor(current_col, cv.COLOR_BGR2GRAY)
     cue = cv.bitwise_and(current, mask)
@@ -96,16 +98,30 @@ while (framenum < lastframe) and (framenum < frame_length - 1):
     #cue_gauss = cv.adaptiveThreshold(cue,250,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
     
     # uget2 counting with countours
-    #render = cv.cvtColor(cue_raw, cv.COLOR_GRAY2BGR)
-    render = current_col.copy()
     contours, hierarchy = cv.findContours(cue_raw, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 	#contours, hierarchy = cv.findContours(cue, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-    for c in contours:
-        cv.fillPoly(render, pts=c, color=(0,255,0))
-    cv.imshow("deteksi", render)
-    vid_cue.write(render)
     
-    print(f'{framenum},{len(contours)}')
+    render = current_col
+    # LOKA: atau pakai overlay yang lebih cantik
+    for c in contours:
+        cv.fillPoly(render, pts=c, color=(23,116,255)) # pumpkin orange
+    
+    
+    #heatmap
+    heatmap= heatmap + (COEF_FADE_IN * cue_raw/250)
+    heatmap= heatmap - COEF_FADE_OUT
+    heatmap= np.clip(heatmap, 0, None)
+    heatmapf = np.clip(heatmap, 0, HEATMAP_CEIL) # saturated heatmap, only for display    
+    heatmapf.itemset((0,0), HEATMAP_CEIL)
+    cv.normalize(heatmapf, heatmap_cue, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1)
+    heatmap_render = cv.applyColorMap(heatmap_cue, cv.COLORMAP_JET)
+    
+    cv.imshow("treshold", cue_raw)
+    cv.imshow("deteksi", render)
+    cv.imshow("heatmap", heatmap_render)
+    #vid_cue.write(render)
+    
+    print(f'{framenum},{len(contours)},{np.min(heatmap)},{np.max(heatmap)}')
     
     framenum= framenum+1
     key = cv.waitKey(1) & 0xff
