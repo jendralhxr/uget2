9e6486e12c42ffca14a74b135cb5b9cf4b31b5b1
yoen <jendral.hxr@gmail.com>
2024-03-18 10:32:03 +0700

    penyakit ga bisa simpen video dari tahap masking
    jalan 6 fps di letop i3-6100

--- trackandcount.py
+++ trackandcount.py
@@ -8,7 +8,7 @@ import cmapy
 COEF_FADE_IN  = 1.00
 COEF_FADE_OUT = 0.75
 HEATMAP_CEIL= COEF_FADE_IN * 60 # persistence presence is noted within 1 second
-threshold_value = 13 # good place to start
+threshold_value = 10 # good place to start
 # buat LOKA: any way to set these values more gracefully? like a slider?
 
 framenum = 0
@@ -19,45 +19,45 @@ cap = cv.VideoCapture(sys.argv[1])
 width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH));
 height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT));
 fps = cap.get(cv.CAP_PROP_FPS);
+fourcc = cv.VideoWriter_fourcc(*'XVID')
 frame_length = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+
 print("input video is: {}x{} @{} {}".format(width, height, fps, frame_length))
 
 startframe = int(sys.argv[2])
 cap.set(cv.CAP_PROP_POS_FRAMES, float(startframe))
 framenum = startframe;
 lastframe = int(sys.argv[3])
-
-vid_cue = cv.VideoWriter(sys.argv[4], cv.VideoWriter_fourcc(*'mp4v'), fps, (640, 480))
-vid_heat = cv.VideoWriter(sys.argv[5], cv.VideoWriter_fourcc(*'mp4v'), fps, (640, 480))
+vid_cue = cv.VideoWriter(sys.argv[4], fourcc, fps, (width, height))
+vid_heat = cv.VideoWriter(sys.argv[5], fourcc, fps, (width, height))
 
 heatmap = np.zeros([height, width], dtype=np.single)
 heatmap_cue = np.full([height, width], 255, dtype=np.uint8)
 tempcount = np.zeros(60, dtype=np.uint)
-
 mask_col = np.full([height, width, 3], (255,255,255), dtype=np.uint8)
 mask = np.full([height, width], 255, dtype=np.uint8)
+key=ord('p');
 
 #------ setting the capilary mask
-key=2;
 color= (0,0,0)
 mask_points=[]
 
+# reference for mask
+ret, current_col = cap.read()
+ref= cv.cvtColor(current_col, cv.COLOR_BGR2GRAY) 
+
+"""
 def mouse_callback(event, x, y, flags, param):
     global mask_col, color
     if event == cv.EVENT_LBUTTONDOWN:
         mask_points.append((x, y))
         #print(f"Clicked at position: ({x}, {y})")
         cv.circle(mask_col, (x,y), 4, (0,200,0), -1)
-
     if len(mask_points) > 2:
         mask_contour= np.array(mask_points)
         cv.fillPoly(mask_col, pts=[mask_contour], color=(0,0,0))
         cv.fillPoly(mask, pts=[mask_contour], color=(0))
-
 cv.setMouseCallback(window_name, mouse_callback)
-
-ret, current_col = cap.read()
-ref= cv.cvtColor(current_col, cv.COLOR_BGR2GRAY) 
 
 # drawing the mask
 while (key != ord('s')):
@@ -69,14 +69,15 @@ while (key != ord('s')):
     elif key==ord('c'): # clear masks
         mask_points=[]
     elif key==ord('s'): # start analysis
+        cv.setMouseCallback(window_name, lambda *args : None)
         cv.destroyAllWindows()    
+        key=ord('-') # unlikely to press this 
         break
+"""
 
 # --- main routine
 while (framenum < lastframe) and (framenum < frame_length - 1):
     ret, current_col = cap.read()
-    #vid_cue.write(current_col)
-    
     current = cv.cvtColor(current_col, cv.COLOR_BGR2GRAY)
     cue = cv.bitwise_and(current, mask)
 
@@ -98,7 +99,7 @@ while (framenum < lastframe) and (framenum < frame_length - 1):
     
     # uget2 counting with countours
     contours, hierarchy = cv.findContours(cue_raw, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-	#contours, hierarchy = cv.findContours(cue, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+    #contours, hierarchy = cv.findContours(cue, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
     
     render = current_col
     # LOKA: atau pakai overlay yang lebih cantik
@@ -119,49 +120,58 @@ while (framenum < lastframe) and (framenum < frame_length - 1):
     
     # current presence
     cmoments= cv.moments(cue_raw, binaryImage=True) 
-    cmx = int(cmoments["m10"] / cmoments["m00"])
-    cmy = int(cmoments["m01"] / cmoments["m00"])
+    mass= cmoments["m00"]
+    if (mass==0):
+        mass=1
+    cmx = int(cmoments["m10"] / mass)
+    cmy = int(cmoments["m01"] / mass)
     cv.circle(heatmap_render, (cmx,cmy), 4, (0,200,0), -1)
     # cumulative heatmap
     hmoments= cv.moments(heatmap)
-    hmx = int(hmoments["m10"] / hmoments["m00"])
-    hmy = int(hmoments["m01"] / hmoments["m00"])
+    hmass= hmoments["m00"]
+    if (hmass==0):
+        hmass=1
+    hmx = int(hmoments["m10"] / hmass)
+    hmy = int(hmoments["m01"] / hmass)
     cv.circle(heatmap_render, (hmx,hmy), 4, (0,0,200), -1)
     
-    cv.imshow("treshold", cue_raw)
-    cv.imshow("deteksi", render)
-    cv.imshow("heatmap", heatmap_render)
-    #vid_cue.write(render)
+    #cv.imshow("treshold", cue_raw)
+    #cv.imshow("deteksi", render)
+    #cv.imshow("heatmap", heatmap_render)
+    vid_cue.write(current_col)
+    vid_heat.write(current_col)
     
     if (framenum%60==0):
-        print(f'{framenum/60},{len(contours)},{np.min(heatmap)},{np.max(heatmap)},{cmx},{cmy},{hmx},{hmy},{int(framenum/60)},{np.average(tempcount)}')
+        print(f'{framenum/60:.3f},{len(contours)},{np.min(heatmap)},{np.max(heatmap)},{cmx},{cmy},{hmx},{hmy},{int(framenum/60)},{np.average(tempcount)}')
         tempcount = np.zeros(60, dtype=np.uint)
     else:
-        print(f'{framenum/60},{len(contours)},{np.min(heatmap)},{np.max(heatmap)},{cmx},{cmy},{hmx},{hmy}')
+        print(f'{framenum/60:.3f},{len(contours)},{np.min(heatmap)},{np.max(heatmap)},{cmx},{cmy},{hmx},{hmy}')
     
     framenum= framenum+1
-    key = cv.waitKey(1) & 0xff
+    #print(framenum)
+    #key = cv.waitKey(1) & 0xff
     if key==27:
         quit()
-    if key==ord('s'):
+    elif key==ord('s'):
         print("save {}".format(framenum))
         cv.imwrite("cue"+str(framenum)+".png", cue)
         cv.imwrite("det"+str(framenum)+".png", cue_raw)
         cv.imwrite("ref"+str(framenum)+".png", ref)
-    if key==ord('a'):
+    elif key==ord('a'):
         threshold_value = threshold_value -1
         print("threshold: "+str(threshold_value))
-    if key==ord('d'):
+    elif key==ord('d'):
         threshold_value = threshold_value +1
         print("threshold: "+str(threshold_value))
-    if key==ord('r'): # reset video from the beginning
+    elif key==ord('r'): # reset video from the beginning
         vid_cue.release()
         vid_heat.release()
-        vid_cue = cv.VideoWriter(sys.argv[4], cv.VideoWriter_fourcc(*'mp4v'), fps, (640, 480))
-        vid_heat = cv.VideoWriter(sys.argv[5], cv.VideoWriter_fourcc(*'mp4v'), fps, (640, 480))
+        vid_cue = cv.VideoWriter(sys.argv[4], fourcc, fps, (frame_width, frame_height))
+        vid_heat = cv.VideoWriter(sys.argv[5], fourcc, fps, (frame_width, frame_height))
         framenum= 0
         cap.set(cv.CAP_PROP_POS_FRAMES, float(0.0))
         
+print("end of processing")
 cap.release
 vid_cue.release
 vid_heat.release
